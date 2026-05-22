b2b3d6b4196e939ccfc309a403df26d461761f49
Maikel-Gudryan <1-25-13318@aluno.faminas.edu.br>
2026-05-22 19:29:46 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import "/src/styles-header.css";
+
+export default function App() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="logo">Prefeitura+</h1>
+
+        <nav className="menu">
+          <a href="/">Início</a>
+          <a href="/">Denúncias</a>
+          <a href="/">Diretório</a>
+
+          <button>Sair</button>
+        </nav>
+      </header>
+    </div>
+  );
+}
